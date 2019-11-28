5500a5419fcf6eaec1a2d939f86a487415f79286
AtshshiMori <atsmr.30autb@gmail.com>
2019-11-28 15:29:20 +0900

    first commit

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate();
+
+    }
+}
