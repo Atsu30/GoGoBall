54adf9089060194e7b33ca6970432028dc66ed89
AtshshiMori <atsmr.30autb@gmail.com>
2019-11-28 16:08:24 +0900

    [add]ボールの操作, カメラの追従機能を追加

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -13,7 +13,11 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
-        transform.Translate();
+        transform.position += transform.forward * speed * Time.deltaTime;
 
+        if (Input.anyKeyDown)
+        {
+            transform.Rotate(new Vector3(0, 90, 0));
+        }
     }
 }
